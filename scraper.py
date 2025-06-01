f712c30d55a6269312ce2e2eb13ec9a1504ea89a
Mehul Jain <62044754+jmehul36@users.noreply.github.com>
2025-06-01 17:19:07 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -15,8 +15,17 @@ import time
 
 CSV_FILE = "out.csv"
 HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
-    'Accept-Language': 'en-US,en;q=0.5'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Connection': 'keep-alive',
+    'DNT': '1',
+    'Upgrade-Insecure-Requests': '1',
+    'Sec-Fetch-Site': 'none',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-User': '?1',
+    'Sec-Fetch-Dest': 'document'
 }
 FIELDS = ["ASIN", "Title", "Brand", "Price", "Rating", "Review Count", "Availability", "Image URL", "Category", "Description"]
 
